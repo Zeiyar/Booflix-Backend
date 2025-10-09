d9930b8478091935d6ba561d89b1c8e9a3fb1ae1
Zeiyar <marieycorentin97@gmail.com>
2025-10-09 10:54:36 +0200

    new page parameter with subscription and possibility of changing password and clearing doublon in home page for the watchlist

--- Routes/Watchlist.js
+++ Routes/Watchlist.js
@@ -6,7 +6,7 @@ router.post("/",async(req,res)=>{
     try{
     const { userId, file, progress, poster, title} = req.body;
 
-    const titled = title.split(".").slice(0,2).trim();
+    const titled = title.split(".").slice(0,2).join(".");
 
     const item = await Watchlist.findOneAndUpdate(
         { userId, titled },
